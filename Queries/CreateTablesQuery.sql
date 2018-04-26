543cf9319f0dbd28766851b0ac8baccaa05573c6
mornado <mornado12@mail.ru>
2018-04-26 03:52:36 +0300

    Queries added.

---
+++ Queries/CreateTablesQuery.sql
@@ -0,0 +1,200 @@
+
+CREATE TABLE [Roles]
+( 
+	[role_id]            integer identity(1,1)  NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	[permission_level]   integer  NULL ,
+	CONSTRAINT [XPKRole] PRIMARY KEY  CLUSTERED ([role_id] ASC)
+)
+go
+
+CREATE TABLE [Users]
+( 
+	[user_id]            integer identity(1,1) NOT NULL ,
+	[login]              nvarchar(20)  NULL ,
+	[pass]               nvarchar(20)  NOT NULL ,
+	CONSTRAINT [XPKUser] PRIMARY KEY  CLUSTERED ([user_id] ASC)
+)
+go
+
+CREATE TABLE [User_Roles]
+( 
+	[role_user_id]       integer identity(1,1) NOT NULL ,
+	[role_id]            integer  NOT NULL ,
+	[user_id]            integer  NOT NULL ,	
+	CONSTRAINT [XPKRole_User] PRIMARY KEY  CLUSTERED ([role_user_id] ASC),
+	CONSTRAINT [R_12] FOREIGN KEY ([role_id]) REFERENCES [Roles]([role_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_13] FOREIGN KEY ([user_id]) REFERENCES [Users]([user_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Departments]
+( 
+	[department_id]      integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	[location]           nvarchar(256)  NULL ,
+	[description]        nvarchar(256)  NULL ,
+	CONSTRAINT [XPKDepartment] PRIMARY KEY  CLUSTERED ([department_id] ASC)
+)
+go
+
+CREATE TABLE [Transfers_Types]
+( 
+	[transfers_type_id]  integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	[description]        nvarchar(256)  NULL ,
+	CONSTRAINT [XPKTransfers_Type] PRIMARY KEY  CLUSTERED ([transfers_type_id] ASC)
+)
+go
+
+CREATE TABLE [Devices_Statuses]
+( 
+	[devices_status_id]  integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	[description]        nvarchar(256)  NULL ,
+	CONSTRAINT [XPKDevices_Status] PRIMARY KEY  CLUSTERED ([devices_status_id] ASC)
+)
+go
+
+CREATE TABLE [Device_Types]
+( 
+	[device_type_id]     integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	CONSTRAINT [XPKDevice_Type] PRIMARY KEY  CLUSTERED ([device_type_id] ASC)
+)
+go
+
+CREATE TABLE [Devices]
+( 
+	[device_id]          integer identity(1,1) NOT NULL ,
+	[devices_status_id]  integer  NOT NULL ,
+	[cost]               money  NULL ,
+	[serial_num]         integer  NOT NULL ,
+	[department_id]      integer  NOT NULL ,
+	[Production_date]    datetime  NULL ,
+	[device_type_id]     integer  NOT NULL ,
+	CONSTRAINT [XPKDevice] PRIMARY KEY  CLUSTERED ([device_id] ASC),
+	CONSTRAINT [R_4] FOREIGN KEY ([devices_status_id]) REFERENCES [Devices_Statuses]([devices_status_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_6] FOREIGN KEY ([department_id]) REFERENCES [Departments]([department_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_25] FOREIGN KEY ([device_type_id]) REFERENCES [Device_Types]([device_type_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Transfers]
+( 
+	[transfer_id]        integer identity(1,1) NOT NULL ,
+	[transfers_type_id]  integer  NOT NULL ,
+	[device_id]          integer  NOT NULL ,
+	[date]               datetime  NULL ,
+	[cost]               money  NULL ,
+	[description]        nvarchar(256)  NULL ,
+	[user_id]            integer  NOT NULL ,
+	CONSTRAINT [XPKTransfer] PRIMARY KEY  CLUSTERED ([transfer_id] ASC),
+	CONSTRAINT [R_17] FOREIGN KEY ([transfers_type_id]) REFERENCES [Transfers_Types]([transfers_type_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_18] FOREIGN KEY ([device_id]) REFERENCES [Devices]([device_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_26] FOREIGN KEY ([user_id]) REFERENCES [Users]([user_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Transfer_Department_Roles]
+( 
+	[transfer_department_role_types_id] integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	CONSTRAINT [XPKTransfer_Department_Role_Types] PRIMARY KEY  CLUSTERED ([transfer_department_role_types_id] ASC)
+)
+go
+
+CREATE TABLE [Transfer_Departments]
+( 
+	[transfer_department_id] integer identity(1,1) NOT NULL ,
+	[department_id]      integer  NOT NULL ,
+	[transfer_id]        integer  NOT NULL ,	
+	[transfer_department_role_types_id] integer  NOT NULL ,
+	CONSTRAINT [XPKDepartment_Transfer] PRIMARY KEY  CLUSTERED ([transfer_department_id] ASC),
+	CONSTRAINT [R_20] FOREIGN KEY ([department_id]) REFERENCES [Departments]([department_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_21] FOREIGN KEY ([transfer_id]) REFERENCES [Transfers]([transfer_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_27] FOREIGN KEY ([transfer_department_role_types_id]) REFERENCES [Transfer_Department_Roles]([transfer_department_role_types_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Managers]
+( 
+	[manager_id]         integer identity(1,1) NOT NULL ,
+	[department_id]      integer  NOT NULL ,
+	[first_name]         nvarchar(20)  NOT NULL ,
+	[last_name]          nvarchar(20)  NOT NULL ,
+	[address]            nvarchar(50)  NULL ,
+	[tel]                nvarchar(12)  NULL ,
+	[dob]                datetime  NULL ,
+	[user_id]            integer  NOT NULL ,
+	CONSTRAINT [XPKManager] PRIMARY KEY  CLUSTERED ([manager_id] ASC),
+	CONSTRAINT [R_7] FOREIGN KEY ([department_id]) REFERENCES [Departments]([department_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_10] FOREIGN KEY ([user_id]) REFERENCES [Users]([user_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Attributes_Types]
+( 
+	[attributes_type_id] integer identity(1,1) NOT NULL ,
+	[name]               nvarchar(50)  NOT NULL ,
+	[units_of_measurement] nvarchar(20)  NULL ,
+	CONSTRAINT [XPKAttributes_Type] PRIMARY KEY  CLUSTERED ([attributes_type_id] ASC)
+)
+go
+
+CREATE TABLE [Device_Type_Attributes]
+( 
+	[device_type_attributes_type_id] integer identity(1,1) NOT NULL ,
+	[device_type_id]     integer  NOT NULL ,
+	[attributes_type_id] integer  NOT NULL ,	
+	CONSTRAINT [XPKDevice_Type_Attributes_Type] PRIMARY KEY  CLUSTERED ([device_type_attributes_type_id] ASC),
+	CONSTRAINT [R_23] FOREIGN KEY ([device_type_id]) REFERENCES [Device_Types]([device_type_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_24] FOREIGN KEY ([attributes_type_id]) REFERENCES [Attributes_Types]([attributes_type_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
+CREATE TABLE [Attributes]
+( 
+	[attribute_id]       integer identity(1,1) NOT NULL ,
+	[attributes_type_id] integer  NOT NULL ,
+	[device_id]          integer  NOT NULL ,	
+	[val]                nvarchar(50)  NOT NULL ,
+	CONSTRAINT [XPKAttributes_Types_Devices] PRIMARY KEY  CLUSTERED ([attribute_id] ASC),
+	CONSTRAINT [R_2] FOREIGN KEY ([attributes_type_id]) REFERENCES [Attributes_Types]([attributes_type_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT [R_3] FOREIGN KEY ([device_id]) REFERENCES [Devices]([device_id])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
---
+++ Queries/CreateTransactionsQuery.sql
@@ -0,0 +1,255 @@
+CREATE TYPE dbo.DeviceAttributesList
+AS TABLE
+(
+  attribute_type_id INT,
+  val nvarchar(50)
+);
+GO
+
+CREATE PROCEDURE AddNewDevice (@type_id int, @dept_id int, @dev_cost money, @serial_num int, @prod_date datetime, 
+@description nvarchar(256), @tran_cost money, @user_id int, @attributes as dbo.DeviceAttributesList READONLY)
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+
+			--Add device
+			INSERT INTO Devices 
+			(devices_status_id, device_type_id, department_id, cost, serial_num, Production_date)
+			VALUES(1, @type_id, @dept_id, @dev_cost, @serial_num, @prod_date);
+
+			--Add device attributes
+			DECLARE @new_dev_id int;
+
+			SELECT @new_dev_id = device_id FROM Devices 
+			WHERE device_type_id = @type_id AND department_id = @dept_id AND cost = @dev_cost 
+			AND serial_num = @serial_num AND Production_date = @prod_date;
+
+			INSERT INTO Attributes (device_id, attributes_type_id, val) 
+			SELECT @new_dev_id, attribute_type_id, val FROM @attributes;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (1, @new_dev_id, GETDATE(), @tran_cost, @description, @user_id)
+
+			--Add transfer dep
+			DECLARE @new_tran_id int;
+
+			SELECT @new_tran_id = transfer_id FROM Transfers 
+			WHERE transfers_type_id = 1 AND device_id = @new_dev_id AND cost = @tran_cost 
+			AND description = @description AND user_id = @user_id;
+
+			INSERT INTO Transfer_Departments (transfer_id, department_id, transfer_department_role_types_id)
+			VALUES (@new_tran_id, @dept_id, 2);
+
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE MoveDevice (@dev_id int, @new_dept_id int, @date datetime, @description nvarchar(256), @user_id int )
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+
+			--Save old department id
+			DECLARE @old_dept_id int;
+			SELECT @old_dept_id = department_id FROM Devices WHERE device_id = @dev_id;
+
+			--Move device
+			UPDATE Devices SET department_id = @new_dept_id WHERE device_id = @dev_id;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (2, @dev_id, @date, 0, @description, @user_id)
+
+			--Add transfer departments
+			DECLARE @new_tran_id int;
+
+			SELECT @new_tran_id = transfer_id FROM Transfers 
+			WHERE transfers_type_id = 2 AND device_id = @dev_id AND date = @date
+			AND cost = 0 AND description = @description AND user_id = @user_id;
+
+			--from
+			INSERT INTO Transfer_Departments (transfer_id, department_id, transfer_department_role_types_id)
+			VALUES (@new_tran_id, @old_dept_id, 1);
+
+			--where
+			INSERT INTO Transfer_Departments (transfer_id, department_id, transfer_department_role_types_id)
+			VALUES (@new_tran_id, @new_dept_id, 2);
+
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE MoveToRestoreDevice (@dev_id int, @cost money, @date datetime, @description nvarchar(256), @user_id int )
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 3 WHERE device_id = @dev_id;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (3, @dev_id, @date, @cost, @description, @user_id)
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE MoveFromRestoreDevice (@dev_id int, @cost money, @description nvarchar(256), @user_id int )
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 1 WHERE device_id = @dev_id;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (2, @dev_id, GETDATE(), @cost, @description, @user_id)
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE SaleDevice (@dev_id int, @cost money, @date datetime, @description nvarchar(256), @user_id int )
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 5 WHERE device_id = @dev_id;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (4, @dev_id, @date, @cost, @description, @user_id)
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE RemoveDevice (@dev_id int, @date datetime, @description nvarchar(256), @user_id int )
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 6 WHERE device_id = @dev_id;
+
+			--Add transfer record
+			INSERT INTO Transfers (transfers_type_id, device_id, date, cost, description, user_id)
+			VALUES (5, @dev_id, @date, 0, @description, @user_id)
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE SetDeviceIsBroken (@dev_id int)
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 2 WHERE device_id = @dev_id;
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE SetDeviceCannotRestore (@dev_id int)
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Change device status
+			UPDATE Devices SET devices_status_id = 4 WHERE device_id = @dev_id;
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE SetDeviceNewAttributeVal (@dev_id int, @attr_type_id int, @val nvarchar(50))
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Add new device attribute
+			INSERT INTO Attributes (device_id, attributes_type_id, val)
+			VALUES (@dev_id, @attr_type_id, @val);			
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE SetDeviceAttributeVal (@dev_id int, @attr_type_id int, @new_val nvarchar(50))
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	BEGIN TRY
+		BEGIN TRAN
+				
+			--Set new value
+			UPDATE Attributes SET val = @new_val 
+			WHERE device_id = @dev_id AND attributes_type_id = @attr_type_id;
+			
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
